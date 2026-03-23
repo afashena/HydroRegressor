87eadc98e08ad1f79aeee2217edeb1313ad7be0e
Aidan Fashena <atfashena@gmail.com>
2026-03-23 09:28:42 -0400

    Made UI more intuitive and add better plotting

--- hydro_model/app/UI/main.py
+++ hydro_model/app/UI/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import altair as alt
 
 from app.db_utils.database import DB
 from app.UI.utils import parse_site_ids
@@ -12,28 +13,73 @@ db = DB()
 
 st.title("🌧️ Hydroregressor Dashboard 🎛️")
 
-rain_site_id = st.sidebar.text_area("Enter the Rain Gage Site IDs: ", value="")
-st.sidebar.write("Selected value:", rain_site_id)
+with st.expander(label="Data Visualization", icon=":material/insert_chart:"):
 
-stream_site_id = st.sidebar.text_area("Enter the Stream Gage Site IDs: ", value="")
-st.sidebar.write("Selected value:", stream_site_id)
+    rain_site_id = st.text_area("Enter the Rain Gage Site IDs: ", value="")
+    st.write("Selected value:", rain_site_id)
 
-if rain_site_id != "":
+    stream_site_id = st.text_area("Enter the Stream Gage Site IDs: ", value="")
+    st.write("Selected value:", stream_site_id)
 
-     # Parse given site IDs
-     rain_site_ids = parse_site_ids(rain_site_id)
-     stream_site_ids = parse_site_ids(stream_site_id)
 
-     # Get data from the DB (normal first, then try cache)
-     rain_gage_data = db.querier.get_data_from_range(table_name="rain_gage_data", site_id=rain_site_ids[0])
+    if rain_site_id != "":
 
-     # Plot data
-     with psycopg.connect(db.updater.conn_info) as conn:
-            with conn.cursor() as cur:
-               all_columns = db.updater.get_table_columns(table_name="rain_gage_data", cur=cur).split(sep=", ")
-               print(all_columns)
-     chart_data = pd.DataFrame(rain_gage_data, columns=all_columns)
-     st.line_chart(chart_data.set_index("collect_date")["rain_amount"])
+        # Parse given site IDs
+        rain_site_ids = parse_site_ids(rain_site_id)
+        #stream_site_ids = parse_site_ids(stream_site_id)
+
+        # Get data from the DB (normal first, then try cache)
+        rain_gage_data = db.querier.get_data_from_range(table_name="rain_gage_data", site_id=rain_site_ids[0])
+
+        # Plot data
+        with psycopg.connect(db.updater.conn_info) as conn:
+                with conn.cursor() as cur:
+                    all_columns = db.updater.get_table_columns(table_name="rain_gage_data", cur=cur).split(sep=", ")
+                    print(all_columns)
+        chart_data = pd.DataFrame(rain_gage_data, columns=all_columns)
+
+        # make an altair chart
+        hover = alt.selection_point(
+            fields=["collect_date"],
+            nearest=True,
+            on="mouseover",
+            empty="none",
+        )
+
+        lines = (
+            alt.Chart(chart_data, title=f"Rain Amount (in) at Site {rain_site_ids[0]}")
+            .mark_line()
+            .encode(
+                x="collect_date",
+                y="rain_amount",
+            )
+        )
+
+        points = lines.transform_filter(hover).mark_circle(size=65)
+
+        tooltips = (
+                alt.Chart(chart_data)
+                .mark_rule()
+                .encode(
+                    # x="yearmonthdate(collect_date)",
+                    # y="rain_amount",
+                    opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+                    tooltip=[
+                        alt.Tooltip("collect_date", title="Date"),
+                        alt.Tooltip("rain_amount", title="Rain Amount (inches)"),
+                    ],
+                )
+                .add_params(hover)
+            )
+
+        data_layer = lines + points + tooltips
+        st.altair_chart(data_layer, use_container_width=True)
+
+with st.expander(label="Database", icon=":material/database:"):
+    st.write('''
+        Here you can do database things.
+    ''')
+    st.button("Update DB", on_click=db.updater.update_db)
 
 if __name__ == "__main__":
     subprocess.run(["streamlit", "run", "app/UI/main.py"])
